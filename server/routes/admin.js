c0fe5100303df9474251fd755b7ef253659b860f
Irving Alexis Pavia Sosa <irving.pavia.sosa@hotmail.com>
2026-05-11 15:05:58 -0600

    Correccion url invitaciones

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -8,9 +8,11 @@ const PDFDocument = require("pdfkit");
 const fs = require("fs");
 
 
-
-const DOMAIN_URL = "http://127.0.0.1:5500";
-const BASE_INVITE_URL = "http://127.0.0.1:5500/public/index.html";
+const BASE_INVITE_URL =
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1"
+    ? "http://127.0.0.1:5500/public/index.html"
+    : "https://golden-profiterole-8523b0.netlify.app/public/index.html";
 
 router.get("/rsvps", (req, res) => {
   const families = readData();
